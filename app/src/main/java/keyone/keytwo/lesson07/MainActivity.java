1c15970900fc6c1602a8cc917705fb683683c70e
MEA <mayer_evgenija@mail.ru>
2021-07-13 08:07:07 +0700

    В MainActivity инициализировала кнопки из макета

--- app/src/main/java/keyone/keytwo/lesson07/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson07/MainActivity.java
@@ -3,6 +3,7 @@ package keyone.keytwo.lesson07;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        Button buttonBack = findViewById(R.id.buttonBack);
+        Button buttonMain = findViewById(R.id.buttonMain);
+        Button buttonFavorite = findViewById(R.id.buttonFavorite);
+        Button buttonSettings = findViewById(R.id.buttonSettings);
     }
 }
