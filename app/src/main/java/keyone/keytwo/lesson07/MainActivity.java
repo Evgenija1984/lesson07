e6f041b71a66759d631f90450bf2134c8c58ed2c
MEA <mayer_evgenija@mail.ru>
2021-07-13 08:35:33 +0700

    Добавила условие - удалять при добавлении

--- app/src/main/java/keyone/keytwo/lesson07/MainActivity.java
+++ app/src/main/java/keyone/keytwo/lesson07/MainActivity.java
@@ -1,9 +1,16 @@
 package keyone.keytwo.lesson07;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ListFragment;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +23,30 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         Button buttonBack = findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         Button buttonMain = findViewById(R.id.buttonMain);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                if (Settings.isDeleteBeforeAdd) {
+                    List<Fragment> fragmentList = fragmentManager.getFragments();
+                    for (int i = 0; i < fragmentList.size(); i++) {
+                        Fragment fragment = fragmentList.get(i);
+                        if (fragment.isVisible()) {
+                            fragmentTransaction.remove(fragment);
+                        }
+                    }
+
+                }
+            }
+        });
         Button buttonFavorite = findViewById(R.id.buttonFavorite);
         Button buttonSettings = findViewById(R.id.buttonSettings);
     }
