fd23411e9591485c005f688b477c78b5f45314d2
MEA <mayer_evgenija@mail.ru>
2021-07-13 17:18:26 +0700

    В класс настройки добавили поля replace

--- app/src/main/java/keyone/keytwo/lesson07/Settings.java
+++ app/src/main/java/keyone/keytwo/lesson07/Settings.java
@@ -5,11 +5,13 @@ public class Settings {
     public static final String SHARED_PREFERENCE_NAME = "FragmentNavigation";
     public static final String IS_BACK_STACK_USED = "UseBackStack";
     public static final String IS_ADD_FRAGMENT_USED = "UseAddFragment";
+    public static final String IS_REPLACE_FRAGMENT_USED = "UseAddFragment";
     public static final String IS_BACK_AS_REMOVE_FRAGMENT = "BackAsRemove";
     public static final String IS_DELETE_FRAGMENT_BEFORE_ADD = "DeleteFragmentBeforeAdd";
 
     public static boolean isBackStack;
     public static boolean isAddFragment;
+    public static boolean isReplaceFragment;
     public static boolean isBackAsRemove;
     public static boolean isDeleteBeforeAdd;
 
